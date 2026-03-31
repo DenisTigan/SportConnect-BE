b36a5b8dee54d367a4e4d47273ca4a8732924494
unknown <contact.crosswears@gmail.com>
2026-03-31 23:17:16 +0300

    userRepo and customUserDetails

---
+++ demo/demo/src/main/java/com/SportConnect/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.SportConnect.demo.repository;
+
+import com.SportConnect.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
