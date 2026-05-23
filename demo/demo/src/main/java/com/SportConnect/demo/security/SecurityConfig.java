d872bbc4f975f3624cb42b263a295cc5be661f01
unknown <contact.crosswears@gmail.com>
2026-05-23 18:43:43 +0300

    cors

--- demo/demo/src/main/java/com/SportConnect/demo/security/SecurityConfig.java
+++ demo/demo/src/main/java/com/SportConnect/demo/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "https://sport-connect-fr-*.vercel.app/"
+                "https://sport-connect-fr-*.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-type"));
